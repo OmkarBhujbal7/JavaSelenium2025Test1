1b6aa02745b17c4f78a65fdd99b73397cb7fc28a
Omkar Bhujbal <bhujbalomkar57@gmail.com>
2025-07-20 12:41:01 +0530

    2 times changes

--- src/p1/class1.java
+++ src/p1/class1.java
@@ -5,5 +5,6 @@ public class class1 {
 	public static void main(String[] args) {
 		System.out.println("P1 c1 world Technocrdits");
 		System.out.println("New line");
+		System.out.println("2 times changes");
 	}
 }
